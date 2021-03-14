1848e0cf2d1b90ca055ea5ebc34fc3aca6db0727
pruthviraj rathod <er.rathod1207@gmail.com>
2021-03-14 00:59:13 -0500

    author-book relation completed

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,15 @@ class Book < ApplicationRecord
   ### validation
 
   validates_presence_of :book, :author_id
+
+
+  #### callbacks
+
+  after_create :author_book_count
+
+  def author_book_count
+  	self.author.book_count = self.author.book_count.nil? ? 1 : self.author.book_count += 1
+	self.author.save
+  end
+  
 end
